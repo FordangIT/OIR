ae83cd9c9407310492cda975e5e91ee30051fe09
FordangIT <fordang0819@gmail.com>
2024-10-17 18:50:24 +0900

    Feat: 로그인 폼

--- fe/components/login/LoginForm.tsx
+++ fe/components/login/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { useState, ChangeEvent, FormEvent } from "react";
 import { useRouter } from "next/router";
-
+import Link from "next/link";
 interface FormData {
   userid: string;
   password: string;
@@ -33,31 +33,50 @@ export default function LoginForm() {
     router.push("/");
   };
   return (
-    <form onSubmit={handleSubmit} className="">
-      <div>
-        <label></label>
-        <input
-          type="text"
-          id="userid"
-          name="userid"
-          value={formData.userid}
-          onChange={handleChange}
-          required
-        ></input>
+    <form
+      onSubmit={handleSubmit}
+      className="w-96 min-h-screen flex flex-col justify-center items-center"
+    >
+      <div className="w-60">
+        <div className="my-10 w-full">
+          <div>
+            <label></label>
+            <input
+              type="text"
+              id="userid"
+              name="userid"
+              value={formData.userid}
+              onChange={handleChange}
+              placeholder="아이디"
+              required
+              className="w-full h-12 border-tossback-gray border-2 rounded-t-xl p-4 text-sm focus:outline-main-orange"
+            ></input>
+          </div>
+          <div>
+            <label></label>
+            <input
+              type="text"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="비밀번호"
+              required
+              className="w-full h-12 border-tossback-gray border-x-2 border-b-2 rounded-b-xl p-4 text-sm  focus:outline-main-orange"
+            ></input>
+          </div>
+          {error && <p className="text-main-red text-sm p-2">{error}</p>}
+        </div>
+        <button
+          type="submit"
+          className="bg-main-green text-white font-semibold w-full h-12 rounded-lg"
+        >
+          로그인
+        </button>
+        <Link href="/signup">
+          <div className="text-gray-500 text-xs p-3 text-center">회원가입</div>
+        </Link>
       </div>
-      <div>
-        <label></label>
-        <input
-          type="text"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        ></input>
-      </div>
-      {error && <p>{error}</p>}
-      <button type="submit">로그인</button>
     </form>
   );
 }
