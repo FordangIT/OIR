ce59258263a22d66084135e04d232728955957fb
FordangIT <fordang0819@gmail.com>
2024-10-23 15:18:19 +0900

    Design: 전체 레이아웃 수정

--- fe/pages/_app.tsx
+++ fe/pages/_app.tsx
@@ -4,8 +4,10 @@ import Layout from "@/components/common/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className="flex justify-center items-center">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
