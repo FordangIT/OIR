5114a8743e4b9cb48a3db87b118369b675c4bc76
FordangIT <fordang0819@gmail.com>
2024-10-23 15:19:40 +0900

    Refactor: 유틸리티 css 추가

--- fe/pages/index.tsx
+++ fe/pages/index.tsx
@@ -1,28 +1,40 @@
+import Link from "next/link";
 import Image from "next/image";
 import Icon from "@/components/common/Icon";
+import TextInfo from "@/components/common/TextInfo";
 export default function Home() {
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full">
       <div className="w-full h-32 flex justify-evenly items-center bg-tossback-gray">
-        <Icon
-          name="send"
-          className="w-12 h-12 p-1 text-main-green bg-white rounded-md"
-        />
-        <Icon
-          name="message"
-          className="w-12 h-12 p-1 text-main-orange bg-white rounded-md"
-        />
-        <Image
-          src="/images/main.jpeg"
-          alt="충전소"
-          width={45}
-          height={55}
-          className="rounded-md"
-        />
-        <Icon
-          name="money"
-          className="w-12 h-12 p-1 text-yellow-400 bg-white rounded-md"
-        />
+        <Link href="/send">
+          <div className="main-icon-wrapper group">
+            <Icon name="send" className="icon-base text-main-green" />
+            <TextInfo text="send" />
+          </div>
+        </Link>
+        <Link href="/inbox">
+          <div className="main-icon-wrapper group">
+            <Icon name="message" className="icon-base text-main-orange " />
+
+            <TextInfo text="message" />
+          </div>
+        </Link>
+
+        <Link href="/points">
+          <div className="main-icon-wrapper group">
+            <Image
+              src="/images/main.jpeg"
+              alt="충전소"
+              width={45}
+              height={55}
+              className="rounded-md"
+            />
+            <TextInfo text="충전" />
+          </div>
+        </Link>
+        <div className="w-12 h-12">
+          <Icon name="money" className="icon-base text-yellow-400 " />
+        </div>
       </div>
     </div>
   );
