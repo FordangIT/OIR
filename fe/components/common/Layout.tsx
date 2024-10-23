dfbcb6613550d509add3083be4ac4b53034af26b
FordangIT <fordang0819@gmail.com>
2024-10-23 16:02:49 +0900

    Refactor: 정렬 유틸리티 생성

--- fe/components/common/Layout.tsx
+++ fe/components/common/Layout.tsx
@@ -1,15 +1,23 @@
 import { ReactNode } from "react";
-
+import Image from "next/image";
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
-      <div className="flex justify-center items-center min-h-screen w-full sm:w-[30rem] bg-white">
+    <div className="w-full col-position">
+      <div className="w-full h-16 border-b-2 row-position">
+        <Image
+          src="/images/text_logo3.jpeg"
+          alt="OIR 메인 로고"
+          width={100}
+          height={44}
+        />
+      </div>
+      <div className="row-position min-h-screen w-full sm:w-[30rem] bg-white">
         {children}
       </div>
-    </>
+    </div>
   );
 }
