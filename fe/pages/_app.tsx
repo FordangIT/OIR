8e4597fe67db04adf794b15c5f61c641eeb21330
FordangIT <fordang0819@gmail.com>
2024-11-06 20:05:32 +0900

    Chore: 리액트 쿼리 실행 및 설정

--- fe/pages/_app.tsx
+++ fe/pages/_app.tsx
@@ -3,15 +3,21 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/common/Layout";
 import { store } from "@/lib/redux/store";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useMemo } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
   return (
     <Provider store={store}>
-      <div className="flex justify-center items-center">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex justify-center items-center">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </QueryClientProvider>
     </Provider>
   );
 }
