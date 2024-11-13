495d9dd5ad5a58b0182037190a6783f799268cb8
FordangIT <fordang0819@gmail.com>
2024-11-13 19:58:47 +0900

    Chore: redux-persist 설치

--- fe/pages/_app.tsx
+++ fe/pages/_app.tsx
@@ -1,23 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/common/Layout";
-import { store } from "@/lib/redux/store";
+import store, { persistor } from "@/lib/redux/store";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useMemo } from "react";
-
+import { PersistGate } from "redux-persist/integration/react";
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(() => new QueryClient(), []);
 
   return (
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <div className="flex justify-center items-center">
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </div>
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <div className="flex justify-center items-center">
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </div>
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   );
 }
