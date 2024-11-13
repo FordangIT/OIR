7866369ead007f079253fb8f10d4d976e0aa41dc
FordangIT <fordang0819@gmail.com>
2024-11-13 19:59:30 +0900

    Design: 아이콘 디자인 변경

--- fe/components/common/Icon.tsx
+++ fe/components/common/Icon.tsx
@@ -5,10 +5,10 @@ import { IoIosSchool } from "react-icons/io";
 import { PiFinnTheHumanFill } from "react-icons/pi";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { FiUsers } from "react-icons/fi";
-import { BsSendPlusFill } from "react-icons/bs";
-import { TbMessage2Heart } from "react-icons/tb";
-import { RiMoneyDollarCircleFill } from "react-icons/ri";
-
+import { BsSendPlus } from "react-icons/bs";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { AiOutlineQuestion } from "react-icons/ai";
+import { MdOutlineMessage } from "react-icons/md";
 interface IconProps {
   name: string;
   className?: string;
@@ -18,13 +18,14 @@ const iconsMap: { [key: string]: IconType } = {
   school: FaSchool,
   grade: IoIosSchool,
   username: FiUsers,
-  userid: CiUser,
+  userId: CiUser,
   password: RiLockPasswordFill,
   repassword: RiLockPasswordFill,
   nickname: PiFinnTheHumanFill,
-  send: BsSendPlusFill,
-  message: TbMessage2Heart,
-  money: RiMoneyDollarCircleFill
+  send: BsSendPlus,
+  message: MdOutlineMessage,
+  money: RiMoneyDollarCircleLine,
+  question: AiOutlineQuestion
 };
 
 const Icon = ({ name, className }: IconProps) => {
