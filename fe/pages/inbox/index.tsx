22c15b9c02f62de1e1536d9cd1b18a9d3a10b7a9
FordangIT <fordang0819@gmail.com>
2024-11-15 13:51:12 +0900

    Remove: 파일 삭제

--- fe/pages/inbox/index.tsx
+++
@@ -1,9 +0,0 @@
-import InboxList from "@/components/inbox/InboxList";
-
-export default function Inbox() {
-  return (
-    <div className="w-full min-h-screen">
-      <InboxList />;
-    </div>
-  );
-}
