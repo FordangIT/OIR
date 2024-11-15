a355dfeb418ce40cecd2a15e79afc4154b7af24e
FordangIT <fordang0819@gmail.com>
2024-11-15 13:49:23 +0900

    Fix: 사용자 메시지 기능

--- fe/lib/api/inbox.tsx
+++ fe/lib/api/inbox.tsx
@@ -1,25 +1,19 @@
 export async function getUserMessage(userId: string) {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/inbox/${userId}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        }
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/inbox/${userId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
       }
-    );
-    if (!res.ok) {
-      const errorResult = await res.json();
-      return errorResult;
     }
+  );
+  const result = await res.json();
 
-    const result = await res.json();
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw new Error("failed to get message");
+  if (!result.success) {
+    throw new Error(res.message);
   }
+  return result;
 }
 
 export async function deleteMessage(messageId: string) {
--- fe/pages/inbox/[userId].tsx
+++ fe/pages/inbox/[userId].tsx
@@ -2,9 +2,8 @@ import InboxList from "@/components/inbox/InboxList";
 
 export default function Inbox() {
   return (
-    <div className="w-full min-h-screen overflow-y-auto bg-tossback-gray">
-      <div className="text-black z-10">받은 편지함</div>
-      <InboxList />;
-    </div>
+    <>
+      <InboxList />
+    </>
   );
 }
