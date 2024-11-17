e41aeb1d200bdde5515aacdfcf1bf7cab6e151a7
FordangIT <fordang0819@gmail.com>
2024-11-17 22:15:05 +0900

    Design: 디자인 변경

--- fe/components/points/PointClicker.tsx
+++ fe/components/points/PointClicker.tsx
@@ -35,42 +35,53 @@ export default function PointClicker() {
   return (
     <>
       <div className="col-position">
-        {Number(count) >= 10 ? (
-          <Image
-            src="/images/open.png"
-            alt="보물상자 open"
-            width={44}
-            height={44}
-            onClick={handleImageClick}
-          />
-        ) : (
-          <Image
-            src="/images/close.png"
-            alt="보물상자 close"
-            width={44}
-            height={44}
-          />
-        )}
-        <div className="w-fit h-10 px-2 mb-5 bg-main-green rounded-xl row-position text-white font-semibold">
+        <div className="w-fit h-fit p-1 bg-main-green rounded-full row-position text-white font-semibold text-sm mb-2">
           {count}
         </div>
-        {state ? (
-          <Image
-            src={`/images/click1.jpeg`}
-            alt="포인트 충전"
-            height={150}
-            width={150}
-            onClick={handleCount}
-          />
-        ) : (
-          <Image
-            src={`/images/click2.jpeg`}
-            alt="포인트 충전"
-            height={150}
-            width={150}
-            onClick={handleCount}
-          />
-        )}
+        <div className="mb-3">
+          {Number(count) >= 10 ? (
+            <Image
+              src="/images/open.jpeg"
+              alt="보물상자 open"
+              width={70}
+              height={70}
+              onClick={handleImageClick}
+            />
+          ) : (
+            <Image
+              src="/images/close.jpeg"
+              alt="보물상자 close"
+              width={60}
+              height={60}
+            />
+          )}
+        </div>
+        <div className="relative">
+          <div
+            className={`absolute right-0 font-bold text-main-red ${
+              state ? "block" : "hidden"
+            }`}
+          >
+            click
+          </div>
+          {state ? (
+            <Image
+              src={`/images/click1.jpeg`}
+              alt="포인트 충전"
+              height={150}
+              width={150}
+              onClick={handleCount}
+            />
+          ) : (
+            <Image
+              src={`/images/click2.jpeg`}
+              alt="포인트 충전"
+              height={150}
+              width={150}
+              onClick={handleCount}
+            />
+          )}
+        </div>
       </div>
     </>
   );
--- fe/components/points/UserPoints.tsx
+++ fe/components/points/UserPoints.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { getUserPoints } from "@/lib/api/points";
+import Image from "next/image";
 
 export default function UserPoints() {
   const { data, error, isLoading } = useQuery("userPoints", getUserPoints);
@@ -12,5 +13,15 @@ export default function UserPoints() {
     return <div>Error: {error.message}</div>;
   }
 
-  return <div>{data?.data} 포인트</div>;
+  return (
+    <div className="row-position">
+      <Image
+        src="/images/point.jpeg"
+        alt="포인트 이미지"
+        height={20}
+        width={20}
+      />
+      <p className="mx-2">{data?.data} 포인트</p>
+    </div>
+  );
 }
