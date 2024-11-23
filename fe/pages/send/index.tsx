5444e7839e654c3beb6a5d8e6ca5fc278efdcbc4
FordangIT <fordang0819@gmail.com>
2024-11-23 11:51:59 +0900

    Fix: SSR 결과 브라우저 캐싱되는 문제 해결

--- fe/pages/send/index.tsx
+++ fe/pages/send/index.tsx
@@ -1,10 +1,14 @@
 import SendForm from "@/components/send/SendForm";
-// import { requireAuthentication } from "@/lib/utils/requireAuthentication";
-// import { GetServerSideProps } from "next";
+import { requireAuthentication } from "@/lib/utils/requireAuthentication";
+import { GetServerSideProps } from "next";
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   return requireAuthentication(context);
-// };
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  context.res.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  return requireAuthentication(context);
+};
 export default function Send() {
   return (
     <div className="col-position h-full w-full">
