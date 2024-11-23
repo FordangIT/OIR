c2c4601205d5224a3e776b9633642096d1969355
FordangIT <fordang0819@gmail.com>
2024-11-23 16:07:34 +0900

    Fix: SSR Hydration 문제 해결 진행중

--- fe/pages/contact/index.tsx
+++ fe/pages/contact/index.tsx
@@ -2,19 +2,20 @@ import ContactForm from "@/components/contact/ContactForm";
 import { requireAuthentication } from "@/lib/utils/requireAuthentication";
 import { GetServerSideProps } from "next";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  context.res.setHeader(
-    "Cache-Control",
-    "no-store, no-cache, must-revalidate, proxy-revalidate"
-  );
-  return requireAuthentication(context);
-};
+export const getServerSideProps: GetServerSideProps = requireAuthentication(
+  async (context) => {
+    // 필요한 props 반환
+    return {
+      props: {} // 필요 시 다른 데이터를 추가
+    };
+  }
+);
 
 export default function Contact() {
   return (
     <>
       <div className="col-position h-full w-full">
-        <div className="py-2 mb-10 text-lg font-semibold">문의하기 </div>
+        <div className="py-2 mb-10 text-lg font-semibold">문의하기</div>
         <ContactForm />
       </div>
     </>
