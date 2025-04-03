2b10cf5e881cbaee53dfe51844fdb254ca08d9b0
FordangIT <fordang0819@gmail.com>
2025-04-03 19:53:14 +0900

    Design: 로고 text 없앰

--- fe/components/common/Nav.tsx
+++ fe/components/common/Nav.tsx
@@ -10,31 +10,26 @@ export default function Nav() {
       <Link href="/">
         <div className="main-icon-wrapper group">
           <Icon name="home" className="icon-base" />
-          <TextInfo text="home" />
         </div>
       </Link>
       <Link href="/timetable">
         <div className="main-icon-wrapper group">
           <Icon name="timetable" className="icon-base" />
-          <TextInfo text="timetable" />
         </div>
       </Link>
       <Link href="/send">
         <div className="main-icon-wrapper group">
           <Icon name="send" className="icon-base" />
-          <TextInfo text="send" />
         </div>
       </Link>
       <Link href={`/inbox/${userId}`}>
         <div className="main-icon-wrapper group">
           <Icon name="message" className="icon-base" />
-          <TextInfo text="message" />
         </div>
       </Link>
       <Link href="/points">
         <div className="main-icon-wrapper group">
           <Icon name="money" className="icon-base" />
-          <TextInfo text="points" />
         </div>
       </Link>
     </div>
